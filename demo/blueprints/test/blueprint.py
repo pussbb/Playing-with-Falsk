d09da531e2a2cea1a4582a499fc91b0be93747de
pussbb <pussbb@gmail.com>
2015-05-06 15:03:30 +0300

    sorry

---
+++ demo/blueprints/test/blueprint.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+from __future__ import unicode_literals, print_function, absolute_import, \
+    division
+
+from flask_app.controller import Controller, get_method
+from flask_app.helpers.app import create_blueprint
+
+
+class Test(Controller):
+    ROUTE_BASE = ''
+
+    @get_method('/hi/')
+    def index(self):
+        return self.render_view("test.html", {})
+
+
+def register_blueprint(app):
+    controllers = [Test]
+    create_blueprint(app, 'test', __name__, controllers=controllers,
+                     register=True)
---
+++ flask_app/controller/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+from __future__ import unicode_literals, print_function, absolute_import, \
+    division
+
+from ._controller import *
+from .wrappers import *
+
