b7f6ecda308c5dcd962330496a25f71c2cafb846
pussbb <pussbb@gmail.com>
2015-07-11 18:17:48 +0300

    init setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Flask-App',
+    version='0.1',
+    packages=['flask_app', 'flask_app.helpers', 'flask_app.controller'],
+    url='https://github.com/pussbb/Playing-with-Falsk',
+    license='',
+    author='pussbb',
+    author_email='pussbb@gmail.com',
+    description=''
+)
